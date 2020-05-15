caab0b200b1fd502b98689adf98965ab5e980299
Snx <59105899+Snnxx@users.noreply.github.com>
2020-05-16 00:31:18 +0200

    Update

--- src/main/java/de/snx/statsapi/addon/topwall/manager/TopwallManager.java
+++ src/main/java/de/snx/statsapi/addon/topwall/manager/TopwallManager.java
@@ -5,6 +5,20 @@ public class TopwallManager {
 
 
 
+                        if(StatsAPI.getRankingManager().getUUID(RankedType.OPENCHESTS, finalI) != null){
+
+        UUID uuid = StatsAPI.getRankingManager().getUUID(RankedType.OPENCHESTS, finalI);
+        String playerName = UUIDFetcher.getName(uuid);
+        int openchests = StatsAPI.getRankingManager().getValues(RankedType.OPENCHESTS, uuid);
+        int rank = finalI;
+        message = StatsAPIAddon.getFileManager().getPlaceholdersFile().getPlaceholderMessage("PLACEHOLDER.TOP.OPENCHESTS");
+        message = message.replace("%RANK%", String.valueOf(finalI));
+        message = message.replace("%PLAYERNAME%", String.valueOf(playerName));
+        message = message.replace("%OPENCHESTS%", String.valueOf(openchests));
+    }else{
+        message = StatsAPIAddon.getFileManager().getPlaceholdersFile().getPlaceholderMessage("PLACEHOLDER.TOP.NOAVAIBLE");
+    }
+
 
 
 }
